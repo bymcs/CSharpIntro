50f454e99d66dbbb52f3c041ccde23b2d28edc30
Mehmet Can ŞAHİN <squalmoney@gmail.com>
2023-05-05 07:20:46 +0300

    Update Program.cs

--- Classes2/Program.cs
+++ Classes2/Program.cs
@@ -9,6 +9,46 @@
 
             productManager.Add();
 
+            Matematik matematik = new Matematik();
+
+            int sonuc = matematik.Topla(10, 10);
+
+            Console.WriteLine(sonuc);
+            
+            int sonuc2 = matematik.ToplaParams(2,2,2,2,2,1);
+
+            Console.WriteLine(sonuc2);
+
         }
+    }
+
+    class Matematik
+    {
+        public int Topla(int sayi1, int sayi2 = 100)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int ToplaParams(params int[] sayilar)
+        {
+
+            //int sum = 0;
+            //foreach (int i in sayilar)
+            //{
+            //    sum = sum + i;
+            //}
+            //return sum;
+
+            return sayilar.Sum();
+
+        }
+
+        public int Cikar(int sayi1, int sayi2 = 100)
+        {
+            return sayi1 - sayi2;
+        }
+
+
+
     }
 }
